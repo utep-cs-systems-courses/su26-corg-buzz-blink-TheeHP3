78116e94f173e7b2fe10d0017f3448159d738905
TheeHP3 <hpedroza@miners.utep.edu>
2026-07-20 17:03:58 -0600

    Modified buzzerMain.c to have four buttons make different volumes of sound. Changed one line on buzzer.c to have |=

--- demos/lab2/buzzer.c
+++ demos/lab2/buzzer.c
@@ -15,7 +15,7 @@ void buzzer_init()
     P2SEL2 &= ~(BIT6 | BIT7);
     P2SEL &= ~BIT7;
     P2SEL |= BIT6;
-    P2DIR = BIT6;               /* enable output to speaker (P2.6) */
+    P2DIR |= BIT6;               /* enable output to speaker (P2.6) */
 }
 
 void buzzer_set_period(short cycles) /* buzzer clock = 2MHz.  (period of 1k results in 2kHz tone) */
--- demos/lab2/buzzerMain.c
+++ demos/lab2/buzzerMain.c
@@ -2,12 +2,51 @@
 #include "libTimer.h"
 #include "buzzer.h"
 
-int main() {
+void switches_init()
+{
+    /* Assume S1-S4 are P2.0-P2.3 */
+
+    P2DIR &= ~(BIT0 | BIT1 | BIT2 | BIT3);   // Inputs
+
+    P2REN |= (BIT0 | BIT1 | BIT2 | BIT3);    // Enable pull resistors
+
+    P2OUT |= (BIT0 | BIT1 | BIT2 | BIT3);    // Pull-up resistors
+}
+
+int main()
+{
     configureClocks();
- 
+
     buzzer_init();
-    buzzer_set_period(20000);	/* start buzzing!!! 2MHz/1000 = 2kHz*/
 
+    /* Speaker starts OFF */
+    buzzer_set_period(0);
 
-    or_sr(0x18);          // CPU off, GIE on
+    switches_init();
+
+    while (1)
+    {
+        if (!(P2IN & BIT0))
+        {
+            buzzer_set_period(2000);
+        }
+        else if (!(P2IN & BIT1))
+        {
+            buzzer_set_period(1500);
+        }
+        else if (!(P2IN & BIT2))
+        {
+            buzzer_set_period(1000);
+        }
+        else if (!(P2IN & BIT3))
+        {
+            buzzer_set_period(500);
+        }
+        else
+        {
+            buzzer_set_period(0);
+        }
+    }
+
+    return 0;
 }
